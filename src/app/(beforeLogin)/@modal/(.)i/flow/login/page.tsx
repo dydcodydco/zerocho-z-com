de30a9eaf7480738b5e93fd3d23c2116a5828b85
asd0905 <dydcodydco@daum.net>
2024-05-11 23:06:00 +0900

    섹션1 - 인터셉팅 라우트

--- src/app/(beforeLogin)/@modal/(.)i/flow/login/page.tsx
+++ src/app/(beforeLogin)/@modal/(.)i/flow/login/page.tsx
@@ -16,7 +16,6 @@ export default function LoginModal() {
 
 	return (
 		<div className={style.modalBackground}>
-			로그인 모달
 			<div className={style.modal}>
 				<div className={style.modalHeader}>
 					<button className={style.closeButton} onClick={onClickClose}>
