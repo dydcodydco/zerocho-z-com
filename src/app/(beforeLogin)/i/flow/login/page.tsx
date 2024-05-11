8fd4d9995b434158d91d6d5f4670a2feb0c70a5b
asd0905 <dydcodydco@daum.net>
2024-05-11 21:03:13 +0900

    섹션1 - css module을 선택한 이유

--- src/app/(beforeLogin)/i/flow/login/page.tsx
+++ src/app/(beforeLogin)/i/flow/login/page.tsx
@@ -1,10 +1,3 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      로그인 페이지
-    </main>
-  );
+export default function Login() {
+	return <main>로그인 페이지</main>;
 }
