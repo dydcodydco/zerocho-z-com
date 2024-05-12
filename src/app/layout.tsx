dad68dbcdfa97a83fdc67f592a78757d010cac23
asd0905 <dydcodydco@daum.net>
2024-05-12 16:30:18 +0900

    섹션1 - 레이아웃 클론하기

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 	return (
 		<html lang='en'>
 			<body className={inter.className}>
-				루트 레이아웃
+				{/* 루트 레이아웃 */}
 				<div>{children}</div>
 			</body>
 		</html>
