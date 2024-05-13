5708bedb30317e7a7efa09d5bfc9b69b4ffcac45
asd0905 <dydcodydco@daum.net>
2024-05-14 05:56:04 +0900

    섹션2 - 반응형 만들기

--- src/app/(afterLogin)/_component/LogoutButton.tsx
+++ src/app/(afterLogin)/_component/LogoutButton.tsx
@@ -14,13 +14,13 @@ export default function LogoutButton() {
 
 	return (
 		<button className={style.logOutButton} onClick={onLogout}>
-			<div className={style.logOutUserImage}>
-				<img src={me.image} alt={me.id} />
-			</div>
-			<div className={style.logOutUserName}>
-				<div>{me.nickname}</div>
-				<div>@{me.id}</div>
-			</div>
-		</button>
+      <div className={style.logOutUserImage}>
+        <img src={me.image} alt={me.id}/>
+      </div>
+      <div className={style.logOutUserName}>
+        <div>{me.nickname}</div>
+        <div>@{me.id}</div>
+      </div>
+    </button>
 	);
 }
