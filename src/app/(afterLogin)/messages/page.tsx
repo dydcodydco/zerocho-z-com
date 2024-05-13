19074b31604ea375028d8eb9c4783b72256a0554
asd0905 <dydcodydco@daum.net>
2024-05-14 04:34:57 +0900

    섹션2 - /messages 페이지

--- src/app/(afterLogin)/messages/page.tsx
+++ src/app/(afterLogin)/messages/page.tsx
@@ -1,3 +1,18 @@
-export default function Page() {
-	return <main>쪽지 페이지</main>;
+import style from './messages.module.scss';
+import Room from "@/app/(afterLogin)/messages/_component/Room";
+
+export default function Home() {
+  return (
+    <main className={style.main}>
+      <div className={style.header}>
+        <h3>쪽지</h3>
+      </div>
+      <Room/>
+      <Room/>
+      <Room/>
+      <Room/>
+      <Room/>
+      <Room/>
+    </main>
+  )
 }
