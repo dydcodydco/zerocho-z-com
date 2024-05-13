ffa034b44ba0a05c3cce174f80e0b3e96f04bc2e
asd0905 <dydcodydco@daum.net>
2024-05-13 10:19:09 +0900

    섹션2 - /compose/tweet 만들기

---
+++ src/app/(afterLogin)/_component/Tweet.tsx
@@ -0,0 +1,7 @@
+export default function Tweet() {
+  return (
+    <div style={{ width: '100dvw', height: '100dvh', position: 'fixed', backgroundColor: 'rgba(0, 0, 0, 0.2)', zIndex: 2, left: 0 }}>
+    트윗 모달-------------------------------------
+  </div>
+  )
+}
--- src/app/(afterLogin)/compose/tweet/page.tsx
+++ src/app/(afterLogin)/compose/tweet/page.tsx
@@ -1,3 +1,12 @@
+import Home from '@/app/(afterLogin)/home/page';
+import Tweet from '../../_component/Tweet';
+
+
 export default function Page() {
-	return <main>Tweet</main>;
+	return (
+		<>
+			<Tweet />
+			<Home />
+		</>
+	);
 }
