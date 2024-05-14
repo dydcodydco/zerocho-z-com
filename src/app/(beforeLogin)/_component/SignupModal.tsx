3cce778a5a2536ad2a0a53a5712e1e7981dcf40b
asd0905 <dydcodydco@daum.net>
2024-05-14 14:23:15 +0900

    섹션3 - 서버컴포넌트에서 Server Acions 사용하기

--- src/app/(beforeLogin)/_component/SignupModal.tsx
+++ src/app/(beforeLogin)/_component/SignupModal.tsx
@@ -1,142 +1,88 @@
-'use client';
+// "use client";
 
 import style from '@/app/(beforeLogin)/_component/signup.module.scss';
-import { useRouter } from 'next/navigation';
-import { ChangeEventHandler, useState } from 'react';
-import { useForm, SubmitHandler } from 'react-hook-form';
-
-// 정확한 타입을 useForm에 명시
-interface FormValues {
-	id: string;
-	nickname: string;
-	password: string;
-	image: FileList;
-}
+// import BackButton from '@/app/(beforeLogin)/_component/BackButton';
+import { redirect } from 'next/navigation';
+import { useFormState, useFormStatus } from 'react-dom';
 
 export default function SignupModal() {
-	const [image, setImage] = useState('');
-	const [imageFile, setImageFile] = useState<File>();
-	const {
-		register,
-		handleSubmit,
-		formState: { errors, isValid, isDirty },
-	} = useForm<FormValues>();
+	// const [state, formAction] = useFormState(onSubmit, { message: null });
+	// const { pending } = useFormStatus();
+	
+	// 이게 server actions!!!
+	const submit = async (formData: FormData) => {
+		'use server';
 
-	const router = useRouter();
-	const onClickClose = () => {
-		router.back();
-		// TODO: 뒤로가기가 /home이 아니면 /home으로 보내기
-	};
+		if (!formData.get('id')) {
+			return { message: 'no_id' };
+		}
+		if (!formData.get('name')) {
+			return { message: 'no_name' };
+		}
+		if (!formData.get('password')) {
+			return { message: 'no_password' };
+		}
+		if (!formData.get('image')) {
+			return { message: 'no_image' };
+		}
 
-	const onChangeImageFile: ChangeEventHandler<HTMLInputElement> = (e) => {
-		e.target.files && setImageFile(e.target.files[0]);
-	};
+		let shouldRedirect = false;
+		try {
+			const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/users`, {
+				method: 'post',
+				body: formData,
+				credentials: 'include', // cookie 전달 위해서
+			});
+			console.log(response);
+			console.log(response.status);
+			if (response.status === 403) {
+				return { message: 'user_exists' };
+			}
+			console.log(await response.json());
+			shouldRedirect = true;
+		} catch (error) {
+			console.error(error);
+			return;
+		}
 
-	const onSubmit: SubmitHandler<FormValues> = (formValues) => {
-		console.log(formValues);
-		const { id, nickname, password, image } = formValues;
-		return;
-		// fetch('http://localhost:9090/api/users', {
-		// 	method: 'post',
-		// 	body: JSON.stringify({
-		// 		id,
-		// 		nickname,
-		// 		image,
-		// 		password,
-		// 	}),
-		// 	credentials: 'include',
-		// })
-		// 	.then((response: Response) => {
-		// 		console.log(response.status);
-		// 		if (response.status === 200) {
-		// 			router.replace('/home');
-		// 		}
-		// 	})
-		// 	.catch((err) => {
-		// 		console.error(err);
-		// 	});
-	};
+		if (shouldRedirect) {
+			redirect('/home'); // redirect는 try/catch문에서 쓰면 안된다.
+		}
+	}
 
 	return (
-		<>
-			<div className={style.modalBackground}>
-				<div className={style.modal}>
-					<div className={style.modalHeader}>
-						<button className={style.closeButton} onClick={onClickClose}>
-							<svg width={24} viewBox='0 0 24 24' aria-hidden='true' className='r-18jsvk2 r-4qtqp9 r-yyyyoo r-z80fyv r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-19wmn03'>
-								<g>
-									<path d='M10.59 12L4.54 5.96l1.42-1.42L12 10.59l6.04-6.05 1.42 1.42L13.41 12l6.05 6.04-1.42 1.42L12 13.41l-6.04 6.05-1.42-1.42L10.59 12z'></path>
-								</g>
-							</svg>
-						</button>
-						<div>계정을 생성하세요.</div>
-					</div>
-					<form onSubmit={handleSubmit(onSubmit)}>
-						<div className={style.modalBody}>
-							<div className={style.inputDiv}>
-								<label className={style.inputLabel} htmlFor='id'>
-									아이디
-								</label>
-								<input
-									id='id'
-									className={style.input}
-									type='text'
-									placeholder=''
-									{...register('id', { required: '아이디를 입력해주세요.', minLength: { value: 3, message: '아이디 최소 3자리' } })}
-								/>
-								{errors.id && <p>{errors.id.message}</p>}
-							</div>
-							<div className={style.inputDiv}>
-								<label className={style.inputLabel} htmlFor='nickname'>
-									닉네임
-								</label>
-								<input
-									id='nickname'
-									className={style.input}
-									type='text'
-									placeholder=''
-									{...register('nickname', { required: '닉네임을 입력해주세요', minLength: { value: 3, message: '닉네임 최소 3자리' } })}
-								/>
-								{errors.nickname && <p>{errors.nickname.message}</p>}
-							</div>
-							<div className={style.inputDiv}>
-								<label className={style.inputLabel} htmlFor='password'>
-									비밀번호
-								</label>
-								<input
-									id='password'
-									className={style.input}
-									type='password'
-									placeholder=''
-									{...register('password', { required: '비밀번호를 입력해주세요', minLength: { value: 3, message: '비밀번호 최소 3자리' } })}
-								/>
-								{errors.password && <p>{errors.password.message}</p>}
-							</div>
-							<div className={style.inputDiv}>
-								<label className={style.inputLabel} htmlFor='image'>
-									프로필
-								</label>
-								<input
-									id='image'
-									className={style.input}
-									type='file'
-									accept='image/*'
-									// onChange={onChangeImageFile}
-									{...register('image', {
-										required: '프로필 이미지는 필수입니다.',
-									})}
-								/>
-								{errors.image && <p>{errors.image.message}</p>}
-							</div>
-						</div>
-						<div className={style.modalFooter}>
-							<button className={style.actionButton} disabled={!isDirty || !isValid}>
-								가입하기
-							</button>
-						</div>
-					</form>
-				</div>
-			</div>
-		</>
-	);
+    <>
+      <div className={style.modalBackground}>
+        <div className={style.modal}>
+          <div className={style.modalHeader}>
+            {/* <BackButton /> */}
+            <div>계정을 생성하세요.</div>
+          </div>
+          <form action={submit}>
+            <div className={style.modalBody}>
+              <div className={style.inputDiv}>
+                <label className={style.inputLabel} htmlFor="id">아이디</label>
+                <input id="id" name="id" className={style.input} type="text" placeholder="" required />
+              </div>
+              <div className={style.inputDiv}>
+                <label className={style.inputLabel} htmlFor="name">닉네임</label>
+                <input id="name" name="name" className={style.input} type="text" placeholder="" required />
+              </div>
+              <div className={style.inputDiv}>
+                <label className={style.inputLabel} htmlFor="password">비밀번호</label>
+                <input id="password" name="password" className={style.input} type="password" placeholder="" required />
+              </div>
+              <div className={style.inputDiv}>
+                <label className={style.inputLabel} htmlFor="image">프로필</label>
+                <input id="image" name="image" required className={style.input} type="file" accept="image/*" />
+              </div>
+            </div>
+            <div className={style.modalFooter}>
+              <button type="submit" className={style.actionButton}>가입하기</button>
+              {/* <div className={style.error}>{showMessage(state?.message)}</div> */}
+            </div>
+          </form>
+        </div>
+      </div>
+    </>)
 }
