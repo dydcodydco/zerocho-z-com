d0faf4b29443098fd2c2aee41f5102dd1059dcbd
asd0905 <dydcodydco@daum.net>
2024-06-02 16:35:58 +0900

    Feat: 팔로우/언팔로우 optimistic update

--- src/app/(afterLogin)/_component/FollowRecommend.tsx
+++ src/app/(afterLogin)/_component/FollowRecommend.tsx
@@ -1,10 +1,69 @@
 'use client';
 
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import style from './followRecommend.module.scss';
 import { User } from '@/models/User';
+import { useCallback } from 'react';
+import { useSession } from 'next-auth/react';
+import { produce } from 'immer';
+import cx from 'classnames';
 
-export default function FollowRecommend({user}: {user: User}) {
-  const onFollow = () => { };
+export default function FollowRecommend({ user }: { user: User }) {
+  const { data: session } = useSession();
+  const followed = !!user.Followers?.find(d => d.userId === session?.user?.email);
+  const queryClient = useQueryClient();
+
+  const follow = useMutation({
+    mutationFn: (userId: string) => {
+      return fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/users/${userId}/follow`, {
+        credentials: 'include',
+        method: 'post',
+      });
+    },
+    onMutate(userId: string) {
+      const value: User[] | undefined = queryClient.getQueryData(['users', 'followRecommends']);
+      if (value) {
+        const index = value.findIndex(v => v.id === userId);
+        const shallow = produce(value, draft => {
+          draft[index].Followers = [...draft[index].Followers, {userId: session?.user?.email as string}];
+          draft[index]._count.Followers += 1;
+        });
+        queryClient.setQueryData(['users', 'followRecommends'], shallow);
+      }
+    },
+    onError(error) {
+      console.log(error);
+    }
+  })
+  const unfollow = useMutation({
+    mutationFn: (userId: string) => {
+      return fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/users/${userId}/follow`, {
+        credentials: 'include',
+        method: 'delete',
+      });
+    },
+    onMutate(userId: string) {
+      const value: User[] | undefined = queryClient.getQueryData(['users', 'followRecommends']);
+      if (value) {
+        const index = value.findIndex(v => v.id === userId);
+        const shallow = produce(value, draft => {
+          draft[index].Followers = draft[index].Followers.filter(d => d.userId !== session?.user?.email);
+          draft[index]._count.Followers -= 1;
+        });
+        queryClient.setQueryData(['users', 'followRecommends'], shallow);
+      }
+    },
+    onError(error) {
+      console.log(error);
+    }
+  })
+  const onFollow = useCallback(() => {
+    if (followed) {
+      unfollow.mutate(user.id);
+    } else {
+      follow.mutate(user.id);
+    }
+  }, [followed, follow, unfollow, user.id]);
   return (
     <div className={style.container}>
       <div className={style.userLogoSection}>
@@ -16,8 +75,8 @@ export default function FollowRecommend({user}: {user: User}) {
         <div className={style.title}>{user.nickname}</div>
         <div className={style.count}>@{user.id}</div>
       </div>
-      <div className={style.followButtonSection}>
-        <button onClick={onFollow}>팔로우</button>
+      <div className={cx(style.followButtonSection, followed && style.followed)}>
+        <button onClick={onFollow}>{followed ? '팔로잉' : '팔로우'}</button>
       </div>
     </div>
   )
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,5 +1,15 @@
+interface UserID {
+  userId: string;
+}
+
 export interface User {
   id: string;
   nickname: string;
   image: string;
+  Followers: UserID[];
+  Followings: UserID[];
+  _count: {
+    Followers: number;
+    Followings: number;
+  }
 }
